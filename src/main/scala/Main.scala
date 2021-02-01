09f0a2b8789db414b44ab7321baa772a9ebbc402
Mark Grechanik <drmark@uic.edu>
2021-02-01 15:15:50 -0600

    added copyright and a few discriminants to gitignore

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,5 +1,23 @@
 
+/*
+ *
+ *   Copyright (c) 2021. Mark Grechanik and Lone Star Consulting, Inc. All rights reserved.
+ *   
+ *   Unless required by applicable law or agreed to in writing, software distributed under  the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the License for the specific language governing permissions and limitations under the License. 
+ *     
+ *     
+ */
+
 object Main {
+
+  trait Car {
+    def drive: Boolean
+  }
+
+  class tesla extends Car {
+    override def drive: Boolean = true
+  }
+
 
   def main(args: Array[String]): Unit = {
     println("Hello world!")
